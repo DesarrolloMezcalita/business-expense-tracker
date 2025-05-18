a3d032006225b2a6945aa7b14319bd3dd7d2ee5b
Marcial Ambriz <marskdev@gmail.com>
2025-05-18 13:51:40 -0600

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: false,
+
+  devtools: { enabled: true },
+
+  modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxt/fonts", "@nuxt/icon"],
+
+  css: ["~/assets/css/main.css"],
+
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  compatibilityDate: "2024-11-27",
+
+  app: {
+    head: {
+      title: "Mi SPA con Nuxt",
+      meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { name: "description", content: "Una app tipo SPA con Nuxt" },
+      ],
+    },
+  },
+});
